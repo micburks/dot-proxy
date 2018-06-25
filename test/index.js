3089655d8a9aa235455038511fa0eeeeb9cda355
Mickey Burks <mickey@burks.online>
2018-06-25 14:07:36 -0500

    Add additional tests

--- test/index.js
+++ test/index.js
@@ -1,18 +1,72 @@
-const assert = require('assert')
-const dotProxy = require('../index.js')
+const dotProxy = require('../lib/index.js')
 
-describe('dotProxy', function () {
-  it('sets a nested path', function () {
-    const target = { a: { b: 1 } }
-    const actual = dotProxy(target)
-    actual['a.b'] = 3
-    const expected = { a: { b: 3 } }
-    assert.deepEqual(actual, expected)
+describe('dotProxy', () => {
+  describe('#set', () => {
+    it('sets a nested path', () => {
+      const expected = { a: { b: 3 } }
+      const actual = dotProxy({
+        a: {
+          b: 1
+        }
+      })
+
+      actual['a.b'] = 3
+
+      expect(actual).to.deep.equal(expected)
+    })
   })
-  it('gets a nested path', function () {
+
+  describe('#get', () => {
+    it('gets a nested path', () => {
+      const expected = 'hello'
+      const target = dotProxy({
+        a: {
+          b: {
+            c: 'hello'
+          }
+        }
+      })
+
+      const withDot = dotProxy(target).a.b.c
+      const withBracket = dotProxy(target)['a.b.c']
+
+      expect(withDot).deep.equal(expected)
+      expect(withBracket).deep.equal(expected)
+    })
   })
-  it('deletes a nested path', function () {
+
+  describe('#delete', () => {
+    it('deletes a nested path', () => {
+      const target = {
+        ralph: {
+          waldo: {
+            emerson: 'nature'
+          }
+        }
+      }
+
+      expect(target.ralph.waldo.emerson).to.equal('nature')
+
+      const proxy = dotProxy(target)
+      delete target['ralph.waldo.emerson']
+
+      expect(target.ralph.waldo.emerson).to.be.undefined
+    })
   })
-  it('has a nested path', function () {
+
+  describe('#has', () => {
+    it('has a nested path', () => {
+      const target = dotProxy({
+        1: {
+          2: {
+            3: 'hello'
+          }
+        }
+      })
+
+      const actual = '1.2.3' in target
+
+      expect(actual).to.be.true
+    })
   })
 })
